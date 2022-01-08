d0abca03054446e2e99101df8c40b2f6f5ec77d8
Daniel Kwon <danielkwon@wustl.edu>
2022-01-07 22:39:24 -0600

    init

---
+++ number-baseball/src/main/java/com/example/numberbaseball/User.java
@@ -0,0 +1,11 @@
+package com.example.numberbaseball;
+
+public class User {
+
+    private final long id;
+    private final String username;
+    private final String password;
+
+    public User ()
+    
+}
